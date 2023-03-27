99335454b48bd81040e2c6402c8e50f902bd1c8e
parjjinhee53 <parkjh6275@naver.com>
2023-03-27 09:32:37 +0900

    로그인, 회원가입 성공

---
+++ ppigcycle/ppigcycle/View/RecycleView.swift
@@ -0,0 +1,20 @@
+//
+//  RecycleView.swift
+//  ppigcycle
+//
+//  Created by Jinhee on 2023/03/26.
+//
+
+import SwiftUI
+
+struct RecycleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecycleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecycleView()
+    }
+}
